4acd61be18f2913c856cc9aafb05f572a70f42de
Palienko.AN <Palienko.AN@dns-shop.ru>
2022-02-20 18:06:23 +1000

    Belated Initial Commit

---
+++ EmployeeAPI/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmployeeAPI.Models
+{
+    public class Employee
+    {
+        public long EmployeeId { get; set; }
+        public string Name { get; set; }
+        public long Department { get; set; }
+        public string Title { get; set; }
+        public long ReportsTo { get; set; }
+        public string HireDate { get; set; }
+    }
+}
---
+++ EmployeeAPI/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
+
+// JSON Serializer
+builder.Services.AddControllersWithViews()
+ .AddNewtonsoftJson(options =>
+     options.SerializerSettings.ReferenceLoopHandling = Newtonsoft
+         .Json.ReferenceLoopHandling.Ignore)
+ .AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver
+     = new DefaultContractResolver());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    // app.UseSwagger();
+    // app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
